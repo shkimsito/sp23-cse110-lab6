d77f8d1ef9663b64c5530af10e38b88219f96357
shkimsito <shkim@tuta.io>
2023-05-22 21:50:49 -0700

    wd

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -7,16 +7,16 @@ class RecipeCard extends HTMLElement {
     super(); // Inheret everything from HTMLElement
 
     // A1. TODO - Attach the shadow DOM to this Web Component (leave the mode open)
-    const shadow = this.attachShadow({ mode: 'open' });
+    let shadow = this.attachShadow({ mode: 'open' });
 
     // A2. TODO - Create an <article> element - This will hold our markup once our data is set
-    const article = document.createElement('article');
+    let article = document.createElement('article');
   
     // A3. TODO - Create a style element - This will hold all of the styles for the Web Component
-    const style = document.createElement('style');
+    let style = document.createElement('style');
 
     // A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made
-    style.innerHTML = `
+    style.textContent = `
     * {
     font-family: sans-serif;
     margin: 0;
@@ -131,19 +131,22 @@ class RecipeCard extends HTMLElement {
     //           do not nest an <article> inside another <article>). You should use Template
     //           literals (template strings) and element.innerHTML for this.
     article.innerHTML = `
-      <img src="${data.imgSrc}"
-        alt="${data.imgAlt}">
-      <p class="title">
-        <a href="${data.titleLnk}">${data.titleTxt}</a>
-      </p>
-      <p class="organization">${data.organization}</p>
-      <div class="rating">
-        <span>${data.rating}</span>
-        <img src="./assets/images/icons/${data.rating}-star.svg" alt="${data.rating} stars">
-        <span>(${data.numRatings})</span>
-      </div>
-      <time>${data.lengthTime}</time>
-      <p class="ingredients">${data.ingredients}</p>`;
+    <img src="${data.imgSrc}"
+      alt="${data.imgAlt}">
+    <p class="title">
+      <a href="${data.titleLnk}">${data.titleTxt}</a>
+    </p>
+    <p class="organization">${data.organization}</p>
+    <div class="rating">
+      <span>${data.rating}</span>
+      <img src="/assets/images/icons/5-star.svg" alt="5 stars">
+      <span>(${data.numRatings})</span>
+    </div>
+    <time>${data.lengthTime}</time>
+    <p class="ingredients">
+      ${data.ingredients}
+    </p>
+    `;
   }
 }
 
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,7 +24,12 @@ function getRecipesFromStorage() {
   // A9. TODO - Complete the functionality as described in this function
   //           header. It is possible in only a single line, but should
   //           be no more than a few lines.
-  return localStorage.getItem('recipes');
+  let recipes = localStorage.getItem('recipes');
+  if (localStorage.length == 0){
+    return [];
+  } else {
+    return JSON.parse(recipes);
+  }
 }
 
 /**
@@ -41,10 +46,11 @@ function addRecipesToDocument(recipes) {
   //            create a <recipe-card> element for each one, and populate
   //            each <recipe-card> with that recipe data using element.data = ...
   //            Append each element to <main>
-  for (const recipe of recipes) {
-    const recipeCard = document.createElement('recipe-card');
-    recipeCard.data = recipe;
-    main.appendChild(recipeCard);
+  for (var recipe of recipes) {
+    let recipeCard = document.createElement('recipe-card');
+    document.write(recipe);
+    recipeCard.data = recipes[recipe];
+    main.append(recipeCard);
   }
 }
 
@@ -58,7 +64,7 @@ function saveRecipesToStorage(recipes) {
   // B1. TODO - Complete the functionality as described in this function
   //            header. It is possible in only a single line, but should
   //            be no more than a few lines.
-  localStorage.setItem('recipes', JSON.stringify(recipes));
+  localStorage.setItem("recipes", JSON.stringify(recipes));
 }
 
 /**
